3b1beb8dca2b5f86a362a0855689a7a265cf7ee1
iller_saver <illersaver@gmail.com>
2024-09-20 23:39:36 +0300

    tweaks

--- src/Pages/TestPage.jsx
+++ src/Pages/TestPage.jsx
@@ -4,27 +4,11 @@ import PageSelector from '../Components/PageSelector.jsx'
 import './TestPage.css'
 
 function TestPage() {
-    const [ping, setPing] = useState();
-
-    async function fetchPing() {
-        try {
-            const response = await fetch('ping');
-            setPing(await response.text());
-        }
-        catch (exc) {
-            console.error('Fetch failed', exc);
-        }
-    }
-
-    useEffect(() => {
-        fetchPing();
-    }, []);
-
     return (
         <div>
             <PageSelector />
             <div className="divBody">
-                <p>response: {ping}</p>
+                <p>Under construction</p>
             </div>
         </div>
     );
