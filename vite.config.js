f23e4965326a055384fb90583e628df1193a296c
iller_saver <illersaver@gmail.com>
2024-09-20 20:30:30 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    base: "/MyWebsite/",
+    base: "",
     server: {
         proxy: {
             '^/ping': {
