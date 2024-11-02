26c44b4bf26d8d0b19316be702cd7f4c9853e12a
iller_saver <illersaver@gmail.com>
2024-11-02 18:39:39 +0300

    Updated clicker
    - Saves
    - Passive income

--- src/Components/Clicker/UpgradeButton.jsx
+++ src/Components/Clicker/UpgradeButton.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './UpgradeButton.css'
 
 function UpgradeButton(props)
 {
     const [cost, setCost] = useState(props.cost);
+
+    useEffect(() => {
+        if (localStorage.hasOwnProperty(props.text)) {
+            setCost(localStorage.getItem(props.text));
+        }
+    }, []);
+
     function buy() {
         if (props.cash - cost >= 0) {
             props.function();
-            setCost((cost * 2.9).toFixed(0));
+            setCost((cost * props.costMultiplier).toFixed(0));
+            localStorage.setItem(props.text, (cost * props.costMultiplier).toFixed(0))
             props.setCash(props.cash - cost);
         }
     }
--- src/Pages/Clicker.jsx
+++ src/Pages/Clicker.jsx
@@ -1,16 +1,39 @@
 import PageSelector from '../Components/PageSelector.jsx'
 import Me from '../assets/Me.gif';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 import './Clicker.css'
 import UpgradeButton from '../Components/Clicker/UpgradeButton.jsx';
 
 function Clicker() {
     const [cash, setCash] = useState(0);
-    const clickingPower = useRef(1)
+    const clickingPower = useRef(1);
+    const passiveIncome = useRef(0);
+
+    useEffect(() => {
+        if (localStorage.hasOwnProperty('cash')) {
+            setCash(parseInt(localStorage.getItem('cash')));
+        }
+
+        if (localStorage.hasOwnProperty('passiveIncome')) {
+            passiveIncome.current = parseInt(localStorage.getItem('passiveIncome'));
+        }
+
+        // console.log(JSON.stringify(localStorage));
+    }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCash(cash + passiveIncome.current);
+            localStorage.setItem("cash", cash + passiveIncome.current);
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [cash]);
 
     function mainButtonClicked(){
         setCash(cash + clickingPower.current);
+        localStorage.setItem("cash", cash + clickingPower.current);
     }
 
     return (
@@ -28,12 +51,26 @@ function Clicker() {
 
                 <div className="divBody2Clicker">
                     <UpgradeButton
-                        text="Click power X2!"
+                        text="Click power X2"
                         cash={cash}
                         setCash={setCash}
                         cost={100}
+                        costMultiplier={2.9}
                         function={() => {
                             clickingPower.current *= 2;
+                            localStorage.setItem("clickingPower", clickingPower.current);
+                        }}>
+                    </UpgradeButton>
+
+                    <UpgradeButton
+                        text="Passive income"
+                        cash={cash}
+                        setCash={setCash}
+                        cost={100}
+                        costMultiplier={1.1}
+                        function={() => {
+                            passiveIncome.current += 5;
+                            localStorage.setItem("passiveIncome", passiveIncome.current);
                         }}>
                     </UpgradeButton>
                 </div>
